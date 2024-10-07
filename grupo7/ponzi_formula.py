61647910a98b7ad718cba0277afb79af7b8b1c6b
Samuel Barbosa <91901544+maccur@users.noreply.github.com>
2024-10-07 13:32:18 -0500

    Delete grupo7 directory

--- grupo7/ponzi_formula.py
+++
@@ -1,9 +0,0 @@
-import sympy as sp
-
-balance_por_referido = sp.symbols('balance_por_referido')
-referidos_nivel, niveles = sp.symbols('referidos_nivel niveles')
-crecimiento_exponencial = 2**niveles
-balance_total = referidos_nivel * crecimiento_exponencial * balance_por_referido
-
-
-print(balance_total.subs({niveles: 3, balance_por_referido: 100}))  # 800
